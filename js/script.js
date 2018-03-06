8722f1a5ca244b028303be1b302eef1393094c07
KELE <kele@masong.me>
2018-03-06 22:30:08 +0800

    pythonic

--- js/script.js
+++ js/script.js
@@ -3,31 +3,29 @@ $(function () {
 
     var userid="loser7758";
     var url=window.location.href;
-    if(url.indexOf("https://github.com/"+userid+"/")!=-1){
-        if(url.indexOf("/settings")==-1){
-            window.location.href=url+"/settings";
-            //$(".btn,.btn-danger,.boxed-action").click();
-        }else{
-            var name=$("strong[itemprop='name'] a").text();
-            $(".form-contro,.input-block").val(name);
-            
-            //var h = $(document).height()-$(window).height();
-            //$(document).scrollTop(h);
+    if(url=="https://github.com/"){
+        window.location.href="https://github.com/"+userid+"?tab=repositories"; 
+    }else{
+        if(url.indexOf("https://github.com/"+userid+"/")!=-1 && url.indexOf("/settings")!=-1){
+                var name=$("strong[itemprop='name'] a").text();
+                $(".form-contro,.input-block").val(name);
+                
+                //var h = $(document).height()-$(window).height();
+                //$(document).scrollTop(h);
 
-            //$(".Box.Box--danger button[aria-expanded='false']")
-            //$(".Box.Box--danger").find("li").eq(3).children("button").click();
-            $(".btn.btn-block.btn-danger").attr("disabled",false);
-            $(".btn.btn-block.btn-danger").click();
+                //$(".Box.Box--danger button[aria-expanded='false']")
+                //$(".Box.Box--danger").find("li").eq(3).children("button").click();
+                $(".btn.btn-block.btn-danger").attr("disabled",false);
+                $(".btn.btn-block.btn-danger").click();
         }
-    }
-    var container=$("#js-flash-container").html();
-    if(container.indexOf("successfully deleted")!=-1){
-         window.location.href="https://github.com/"+userid+"?tab=repositories";   
-    }
-    if(url=="https://github.com/"+userid+"?tab=repositories"){
-         var f=$('a[itemprop="name codeRepository"]')[0].href;
-         //console.log(f);
-         window.location.href=f+"/settings";    
+        var container=$("#js-flash-container").html();
+        if(container.indexOf("successfully deleted")!=-1){
+             window.location.href="https://github.com/"+userid+"?tab=repositories";   
+        }
+        if(url=="https://github.com/"+userid+"?tab=repositories"){
+             var f=$('a[itemprop="name codeRepository"]')[0].href;
+             window.location.href=f+"/settings";    
+        }
     }
     
 });
